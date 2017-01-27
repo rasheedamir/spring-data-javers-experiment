1ca75df5ee50cc8a5790fdb46a0f761dc52a67f3
Rasheed Amir <rasheed@aurorasolutions.io>
2017-01-27 22:04:25 +0100

    first commit

---
+++ src/main/java/com/as/javers/test/JaversApplication.java
@@ -0,0 +1,13 @@
+package com.as.javers.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaversApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(JaversApplication.class, args);
+	}
+}
